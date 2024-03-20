f2350e135c053aa235bb371aeda8ce22d74cc0fa
Kaliguth <einav846@gmail.com>
2024-03-20 19:39:14 +0200

    Modified footer bootstrap for icons

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,21 +3,28 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div className="bg-light fixed-bottom pt-3">
+    <div className="bg-light fixed-bottom pt-2">
       <p className="bottom-0 start-0">
         <b>All rights reserved to Einav Kohavi 2024 ©</b>
       </p>
       <div className="position-absolute bottom-0 end-0">
-        <Link to="https://github.com/Kaliguth" className="d-inline-flex">
+        <Link
+          to="https://github.com/Kaliguth"
+          className="d-inline-flex align-middle"
+        >
           <img src="github.png" alt="github" width="39" height="39" />
-          <p className="text-info mb-0 pt-1"><b>My Github</b></p>
+          <p className="text-info mb-0 pt-1">
+            <b>My Github</b>
+          </p>
         </Link>
         <Link
           to="https://www.linkedin.com/in/einav-kohavi-9983091b2/"
-          className="d-inline-flex"
+          className="d-inline-flex align-middle"
         >
           <img src="linkedin.png" alt="linkedin" width="45" height="45" />
-          <p className="text-info mb-0 pt-2"><b>My Linkedin</b></p>
+          <p className="text-info mb-0 pt-2">
+            <b>My Linkedin</b>
+          </p>
         </Link>
       </div>
     </div>
